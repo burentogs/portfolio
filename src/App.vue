04b252ba7b32114141735acece2bf9cf5142c3bd
burentogs <j.burentogs@gmail.com>
2022-12-12 23:00:13 +0800

    Background image & menu

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,38 @@
 <template>
-  <nav class="dark:bg-gray-400 pt-1">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-    <font-awesome-icon
-      :icon="['fas', 'toggle-on']"
-      v-if="isDarkMode"
-      @click="toggleDarkMode"
-    />
-    <font-awesome-icon
-      :icon="['fas', 'toggle-off']"
-      v-if="!isDarkMode"
-      @click="toggleDarkMode"
-    />
-  </nav>
-  <router-view />
+  <div class="h-screen">
+    <div
+      class="sm:bg-background-image bg-no-repeat bg-cover h-max bg-background-image-sm"
+    >
+      <div class="flex h-screen justify-center items-center">
+        <div class="text-center bg-blue-400">
+          <router-view class="bg-gray-300" />
+        </div>
+      </div>
+
+      <nav class="mb-10 ml-10 absolute bottom-0 left-0 text-white">
+        <div class="grid grid-cols-1 font-mono">
+          <router-link to="/" class="hover:text-yellow-400"
+            >New Game</router-link
+          >
+          <router-link to="/about" class="pt-1 hover:text-yellow-400"
+            >Find Servers</router-link
+          >
+          <router-link to="/about" class="pt-1 hover:text-yellow-400"
+            >Options</router-link
+          >
+          <router-link to="/about" class="pt-1 hover:text-yellow-400"
+            >Quit</router-link
+          >
+        </div>
+      </nav>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      isDarkMode: false,
-    };
-  },
-  mounted() {
-    this.isDarkMode = window.matchMedia(
-      "(prefers-color-scheme: light)"
-    ).matches;
-  },
-  methods: {
-    toggleDarkMode() {
-      this.isDarkMode = !this.isDarkMode;
-      if (this.isDarkMode) {
-        document.documentElement.classList.add("dark");
-      } else {
-        document.documentElement.classList.remove("dark");
-      }
-    },
-  },
+  data() {},
+  mounted() {},
+  methods: {},
 };
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
   darkMode: "class", // or 'media' or 'class'
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "background-image": "url('../assets/cs-image.svg')",
+        "background-image-sm": "url('../assets/cs-image-sm.svg')",
+      },
+    },
   },
   plugins: [],
 };
